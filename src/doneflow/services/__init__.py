537bb31ffa45e4eb8bded9b0144ffebaa2c3b865
DenivaldoCruz <denivaldo.cruz@gmail.com>
2026-05-28 20:44:26 -0300

    Implement task service orchestration

--- src/doneflow/services/__init__.py
+++ src/doneflow/services/__init__.py
@@ -3,5 +3,12 @@
 from doneflow.services.ai_categorization_service import AICategorizationService
 from doneflow.services.categorization_cache import CategorizationCache
 from doneflow.services.prompt_builder import PromptBuilder
+from doneflow.services.task_service import TaskNotFoundError, TaskService
 
-__all__ = ["PromptBuilder", "AICategorizationService", "CategorizationCache"]
+__all__ = [
+    "PromptBuilder",
+    "AICategorizationService",
+    "CategorizationCache",
+    "TaskService",
+    "TaskNotFoundError",
+]
